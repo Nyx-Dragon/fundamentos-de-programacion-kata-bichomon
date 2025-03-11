91929b35cea466dc6391414b262d95a769245238
Ruben-Garrido-Piquer <rubengarrido241@gmail.com>
2025-03-11 12:20:37 +0100

    Pokedex

--- scripts.js
+++ scripts.js
@@ -7,28 +7,33 @@ parrafo.textContent = 'Generasión 1 Pokimon';
 const elemento = document.getElementById("gen1");
 elemento.style.backgroundColor = "#bc5ae9"; 
 
-console.log(document.title);
-
 //Enlace Pokedex
 console.log(window.location.href)
+// console.log(document.ULR) es lo mismo
 
 //Enlace Dominio
 console.log(window.location.hostname)
 
-const para = document.getElementsByClassName("infocard");
+//Nodos
+console.log(document.querySelectorAll("img"))
+
+//Tambien funciona
+/* const para = document.getElementsByClassName("infocard");
 const node = document.getElementsByClassName("infocard-lg-img")
 
-console.log(node)
+console.log(node) */
 //Cambiar TODAS las imagenes por otras, URL usando un recorrido bucle for
 let images = document.getElementsByClassName("img-fixed");
     for (let i = 0; i < images.length; i++) {
         images[i].src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
     }
-//Para cambiar TODOS los stilos haz la misma funcion bucle colocando 
-// la extension correspondiente que pueden ser varias a la vez .style.background
-const fly = document.getElementsByClassName("itype flying")
+//Para cambiar TODOS los stilos haz la misma funcion bucle colocando en
+// la extension correspondiente, modifiv¡calo con varios parametros varias a la vez 
+// .style.background
+const pokemon = document.querySelectorAll(".infocard")
 
-let volador = document.getElementsByClassName("itype flying");
-    for (let i = 0; i < fly.length; i++) {
-        fly[i].style.background = "blue";
-    }
+    pokemon.forEach((pokemon) => {
+        if (pokemon.innerHTML.includes("Flying")) {
+            pokemon.style.backgroundColor = "blue"
+        }
+    })
