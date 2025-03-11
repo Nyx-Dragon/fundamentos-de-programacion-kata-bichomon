c3bc85bcf645ef292bde7a32d44002372ce81dd3
Ruben-Garrido-Piquer <rubengarrido241@gmail.com>
2025-03-11 11:31:37 +0100

    Pokedex

--- scripts.js
+++ scripts.js
@@ -1,1 +1,25 @@
 console.log(document.title);
+
+//Enlace Pokedex
+console.log(window.location.href)
+
+//Enlace Dominio
+console.log(window.location.hostname)
+
+const para = document.getElementsByClassName("infocard");
+const node = document.getElementsByClassName("infocard-lg-img")
+
+console.log(node)
+//Cambiar TODAS las imagenes por otras, URL usando un recorrido bucle for
+let images = document.getElementsByClassName("img-fixed");
+    for (let i = 0; i < images.length; i++) {
+        images[i].src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif";
+    }
+//Para cambiar TODOS los stilos haz la misma funcion bucle colocando 
+// la extension correspondiente que pueden ser varias a la vez .style.background
+const fly = document.getElementsByClassName("itype flying")
+
+let volador = document.getElementsByClassName("itype flying");
+    for (let i = 0; i < fly.length; i++) {
+        fly[i].style.background = "blue";
+    }
